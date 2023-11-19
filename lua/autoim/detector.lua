25ae1742bf308c50bccab7418d1b0bcc90cc3be6
Eatsolx <eatsol.x@gmail.com>
2023-11-19 21:20:17 +0800

    test

--- lua/autoim/detector.lua
+++ lua/autoim/detector.lua
@@ -5,9 +5,52 @@ local M = {}
 
 -- 函数：检测文本是否包含注释
 function M.hasComment(line)
-  local comment_pattern = "%s*[%#%-%/%*]"
-  return string.match(line, comment_pattern) ~= nil
+  -- 匹配单行注释
+  local single_line_comment_pattern = "%s*[%#%-%/]"
+
+  -- 匹配多行注释的开始和结束标记
+  local multi_line_comment_start = {
+    C = "%s*/%*",
+    Java = "%s*/%*",
+    JavaScript = "%s*/%*",
+    Python = '%s*"""',
+    HTML = "<!%-%-",
+    CSS = "/%*",
+    Bash = "%s*:'",
+    Swift = "%s*/%*",
+    Go = "%s*/%*"
+    -- 添加其他语言的多行注释开始标记
+  }
+
+  local multi_line_comment_end = {
+    C = "%*/%s*",
+    Java = "%*/%s*",
+    JavaScript = "%*/%s*",
+    Python = '%s*"""',
+    HTML = "%-%->",
+    CSS = "%*/",
+    Bash = "'%s*",
+    Swift = "%*/%s*",
+    Go = "%*/%s*"
+    -- 添加其他语言的多行注释结束标记
+  }
+
+  -- 检查是否包含单行注释
+  if string.match(line, single_line_comment_pattern) then
+    return true
+  end
+
+  -- 检查是否包含多行注释标记
+  for lang, start_pattern in pairs(multi_line_comment_start) do
+    local end_pattern = multi_line_comment_end[lang]
+    if string.match(line, start_pattern) or string.match(line, end_pattern) then
+      return true
+    end
+  end
+
+  return false
 end
+
 
 -- 函数：判断上次输入法是否为中文
 function M.isChineseInputMethod()
